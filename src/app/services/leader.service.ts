0fbd531bc7cc55429f74c9a4bb201900377b9a34
utsav04-code <utsavroy44@gmail.com>
2020-09-07 22:44:33 +0530

    Assignment 2

---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LEADERS } from '../shared/leaders';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor() { }
+
+  getLeader() : Leader[]
+  {
+    return LEADERS;
+  }
+
+  getFeaturedLeader(): Leader {
+    return LEADERS. filter((lead) => (lead.featured))[0];
+  }
+}
